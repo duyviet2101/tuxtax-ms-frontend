594ec20c5a0c6a773cdec2db1defb4e988b36347
duyviet2101 <whoisduyviet@gmail.com>
2024-11-22 05:28:01 +0700

    checkout client

--- src/client/ClientCheckout.jsx
+++ src/client/ClientCheckout.jsx
@@ -12,6 +12,7 @@ export default function ClientCheckout() {
     defaultValue: null,
   });
   const [order, setOrder] = useState(null);
+  const [checkoutMethod, setCheckoutMethod] = useState("banking");
 
   const fetchOrder = async () => {
     try {
@@ -25,6 +26,24 @@ export default function ClientCheckout() {
   useEffect(() => {
     fetchOrder();
   }, []);
+
+  const onPay = async () => {
+    const confirm = window.confirm("Xác nhận thanh toán?");
+    if (!confirm) return;
+
+    if (checkoutMethod === "cash") {
+
+    } else {
+      try {
+        const res = await axios.post(`/checkout/create-payment-url`, {
+          orderId: orderId
+        });
+        window.location.href = res.data;
+      } catch (error) {
+        pushToast(error?.response?.data?.message || error?.message, "error");
+      }
+    }
+  }
 
   if (!order) return null;
 
@@ -60,36 +79,32 @@ export default function ClientCheckout() {
             <h1
               className={"text-2xl font-bold"}>{formatVND(order.products.reduce((acc, item) => acc + item.price * item.quantity, 0))}</h1>
           </div>
-          <hr className={"my-2 border-0 border-dashed border-b-2 border-b-gray-400"}/>
+          <h1 className={"font-bold mt-4 text-lg"}>Chiết khấu</h1>
           <div className={"flex justify-between items-center"}>
-            <h1 className={"text font-bold"}>Hình thức thanh toán</h1>
-            <Select className={""} id={"checkout_method"} name={"checkout_method"}>
-              <option value={1}>Tiền mặt</option>
-              <option value={2}>Chuyển khoản</option>
-            </Select>
+            {order?.discounts?.map((item, index) => (
+              <div className={"flex justify-between items-center"} key={index}>
+                <h1 className={""}>{item.reason}</h1>
+                <h1 className={""}>{formatVND(item.value)}</h1>
+              </div>
+            ))}
+            {order?.discounts?.length === 0 && <h1 className={""}>Không có chiết khấu</h1>}
           </div>
           <hr className={"my-2 border-0 border-dashed border-b-2 border-b-gray-400"}/>
-          <div>
-            <h1 className={"text font-bold"}>Thông tin chuyển khoản</h1>
-            <div className={"flex mt-4"}>
-              <img src={"/QR_Checkout.png"} alt={"QR CHECK OUT"} className={"h-40"}/>
-              <div className={"flex justify-center items-center flex-col w-full"}>
-                <h1 className="text-center text-lg">
-                  Techcombank
-                </h1>
-                <h1 className="text-center text-lg">
-                  STK: 1015308512
-                </h1>
-                <h1 className="text-center text-lg">
-                  CTK: HOANG THI THUY
-                </h1>
-              </div>
-            </div>
-          </div>
+          {!order.isPaid && <div className={"flex justify-between items-center"}>
+            <h1 className={"text font-bold"}>Hình thức thanh toán</h1>
+            <Select className={""} id={"checkout_method"} name={"checkout_method"}
+                    value={checkoutMethod}
+                    onChange={(e) => setCheckoutMethod(e.target.value)}
+            >
+              {/*<option value={"cash"}>Tiền mặt</option>*/}
+              <option value={"banking"}>Chuyển khoản</option>
+            </Select>
+          </div>}
           <div className={"mt-4"}>
-            <Button className={"w-full"} onClick={() => {
-              pushToast("Nhân viên sẽ đến kiểm tra, vui lòng đợi!", "success")
-            }}>Thanh toán</Button>
+            <Button className={"w-full"}
+                    disabled={order.isPaid}
+                    onClick={onPay}
+            >{order.isPaid ? "Đã thanh toán" : "Thanh toán"}</Button>
           </div>
         </div>
       </div>
--- src/comps/Bill.jsx
+++ src/comps/Bill.jsx
@@ -1,5 +1,4 @@
 import moment from "moment";
-import {Table} from "flowbite-react";
 import {formatVND} from "../helpers/parsers.js";
 import {forwardRef} from "react";
 

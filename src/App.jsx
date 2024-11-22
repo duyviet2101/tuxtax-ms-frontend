17e69a287f3db8cb9bc2344b9fa42484ab5544cf
duyviet2101 <whoisduyviet@gmail.com>
2024-11-22 13:39:57 +0700

    fix home page

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,11 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path={"/auth/login"} element={<Login/>} />
+            <Route path="/admin" element={
+              <ProtectedRoute>
+                <AdminHome />
+              </ProtectedRoute>}
+            />
             <Route
               path="/admin"
               element={
@@ -49,7 +54,6 @@ function App() {
                 </ProtectedRoute>
               }
             >
-              <Route path="" element={<AdminHome />} />
               <Route path="payment-success" element={<PaymentSuccess />} />
               <Route path="dashboard" element={<AdminDashboard />} />
               <Route path="notifications" element={<AdminNotification />} />

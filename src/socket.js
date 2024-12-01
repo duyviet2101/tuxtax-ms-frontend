fe1b7757c53a3884f5f3650751ff490742567547
duyviet2101 <whoisduyviet@gmail.com>
2024-12-02 02:52:45 +0700

    url fix

--- src/socket.js
+++ src/socket.js
@@ -6,7 +6,7 @@
 
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:3056";
+const URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:3056";
 
 // Ensure the socket is instantiated only once
 let socket;
